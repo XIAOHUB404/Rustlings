16e8b97c7e71315f2c8dc969f86a0c77907ba581
xiaohub404 <xiaohub404>
2025-01-17 15:59:57 +0000

    update

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -8,19 +8,20 @@
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::convert::TryInto;
 
 fn main() {
     let original_price = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> {
+fn sale_price(price: i32) -> u32{
     if is_even(price) {
-        price - 10
+        (price - 10).try_into().unwrap()
     } else {
-        price - 3
+        (price - 3).try_into().unwrap()
     }
+
 }
 
 fn is_even(num: i32) -> bool {
